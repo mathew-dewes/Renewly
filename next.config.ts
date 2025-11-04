25056683ec3459e1f07f69caf8bc9adb9376c0ee
Mathew Dewes <mathewdewes01@gmail.com>
2025-11-05 07:51:54 +1300

    Changes to next config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,13 @@ const nextConfig: NextConfig = {
 
     ],
   },
+    outputFileTracingIncludes: {
+
+       '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+
+       '/*': ['./node_modules/.prisma/client/**/*'],
+
+   },
 };
 
 export default nextConfig;
