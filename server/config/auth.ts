63f82633979f17c3ec5c6f564bc71449efcbb6f9
Mathew Dewes <mathewdewes01@gmail.com>
2025-11-05 07:44:33 +1300

    Conflict fix

--- server/config/auth.ts
+++ server/config/auth.ts
@@ -3,11 +3,6 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma"
 import { nextCookies } from "better-auth/next-js";
 import prisma from "../db/prisma";
-<<<<<<< HEAD
-=======
-
->>>>>>> 3bcaab08b0bf42511091586085e0979c72928ecd
-
 export const auth = betterAuth({
   database: prismaAdapter(prisma,
     { provider: "postgresql" }),
