99b7871a18ad1e2f05af27c58d0295e4f674c845
Mathew Dewes <mathewdewes01@gmail.com>
2025-11-05 07:40:10 +1300

    Changes to prisma client

--- server/config/auth.ts
+++ server/config/auth.ts
@@ -1,10 +1,9 @@
 
-import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma"
-
 import { nextCookies } from "better-auth/next-js";
-const prisma = new PrismaClient()
+import prisma from "../db/prisma";
+
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma,
