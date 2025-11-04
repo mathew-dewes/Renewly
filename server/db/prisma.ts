b3db5d7e3b7769332c019e24e3a7f975e4d90619
Mathew Dewes <mathewdewes01@gmail.com>
2025-11-04 17:08:50 +1300

    Changes to json

--- server/db/prisma.ts
+++ server/db/prisma.ts
@@ -13,3 +13,6 @@ const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAcceler
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
+
+
+
