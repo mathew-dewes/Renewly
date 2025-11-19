7abc1b112c9685fd62b7471e36f16371caff4fe5
Mathew Dewes <mathewdewes01@gmail.com>
2025-11-19 13:10:49 +1300

    Search bar fixed

--- app/assets/_components/SearchBar.tsx
+++ app/assets/_components/SearchBar.tsx
@@ -2,10 +2,10 @@
 export default function SearchBar(){
     
     return (
-           <div className="bg-white rounded-lg border border-gray-200 p-4 mt-5 max-w-120">
+           <div className="bg-white rounded-lg border border-gray-200 p-4 mt-5 sm:max-w-120">
                                 <form className="flex gap-2 " action="/assets" method="GET">
                                     <input type="text" name="query" placeholder="Search assets..."
-                                        className="flex-1 px-4 py-2 border border-gray-300 rounded text-sm md:text-base focus:border-transparent" />
+                                        className="flex-1 px-4 py-2 border border-gray-300 w-full rounded text-sm md:text-base focus:border-transparent" />
                                     <button className={`bg-blue-accent-500 text-light-500 px-3 md:px-8 py-2 rounded-lg cursor-pointer hover:font-semibold`}>Search</button>
                         
                                 </form>
